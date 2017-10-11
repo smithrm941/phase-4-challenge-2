4bbdf7a4b1232a840f93e7ed839fcb94cd959299
smithrm941 <smithrm941@gmail.com>
2017-10-11 00:05:47 -0700

    front end page for new reviews

--- src/routes/albums.js
+++ src/routes/albums.js
@@ -16,4 +16,22 @@ albums.get('/:albumID', (req, res) => {
   })
 })
 
+albums.get('/:albumID/reviews/new', (req, res) => {
+  const albumID = req.params.albumID
+
+  db.getAlbumsByID(albumID, (error, albums) => {
+    if (error) {
+      res.status(500).render('error', {error})
+    } else {
+      const album = albums[0]
+      console.log('what album is this????', album)
+      //placeholder until sign-in is added:
+      res.render('new_review', {album})
+      // db.addNewReview(album.id, (error, reviews) => {
+      //   res.render('new_review', {album, reviews})
+      // })
+    }
+  })
+})
+
 module.exports = albums
